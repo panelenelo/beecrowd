3c7945cbf14948ca2cec27fb9431201491019f92
panelenelo <panelenelo@gmail.com>
2025-08-05 21:34:37 -0300

    study 2025/08/05

--- beginner/1633.cpp
+++ beginner/1633.cpp
@@ -1,18 +1,31 @@
 #include <stdio.h>
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 int main(){
 
     int n, t, c;
+    map<int,int> proc;
 
-    while(!EOF){
-        cin >> n;
-        while(n--){
-            
+    while(cin >> n){
+        long long int total=0;
+        do{
+            cin >> t >> c;
+            cout << t << " " << c << endl;
+            proc.insert(pair(c,t));
+        }while(n--);
+
+        for(auto i : proc){
+            cout << i.first << " - " << i.second << endl;
         }
+
+        proc.clear();
+        cout << "---------------" << endl;
     }
+
+    
 
     return 0;
 }
